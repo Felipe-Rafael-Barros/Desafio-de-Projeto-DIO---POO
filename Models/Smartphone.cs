9363d905e6197e928e8915659bf62aa060ea86f2
Felipe-Rafael-Barros <gitfelipebarros@gmail.com>
2024-11-04 14:25:35 -0300

    Add files via upload

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -5,10 +5,18 @@ namespace DesafioPOO.Models
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        private string Modelo { get; set; }  //Adicionei as 3 propriedades que faltavam
+        private string IMEI { get; set; }  
+        private int Memoria { get; set; }  
+
+        public Smartphone(string numero, string modelo, string imei,int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+
+            Modelo = modelo;  //Criados os 3 parâmetros de construção restantes
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
